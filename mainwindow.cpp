a874a52ff4c98f724e00dd2bb4eed938d8471790
lolsatbirds <lolsatbirds@gmail.com>
2015-07-13 16:51:03 -0400

    Please work this time.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,71 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    updater = new FClientUpdater();
+    setupWindow();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setupWindow()
+{
+
+    if (updater->clientExists() == true)
+    {
+        ui->updateButton->setText("Update");
+    }
+    else
+    {
+
+        ui->updateButton->setText("Download");
+    }
+
+    ui->dVersionLabel->setText(updater->getDLClientVersion());
+    ui->cVersionLabel->setText((updater->getCRClientVersion()));
+}
+
+void MainWindow::on_updateButton_clicked()
+{
+
+    if (updater->clientExists())
+    {
+
+        if (updater->isCurrentClient())
+        {
+
+            return;
+        }
+        else
+        {
+
+            updater->updateClient();
+            ui->updateButton->setText("Update");
+            ui->dVersionLabel->setText(updater->getDLClientVersion());
+        }
+    }
+    else
+    {
+
+        updater->downloadClient();
+        ui->updateButton->setText("Update");
+    }
+}
+
+void MainWindow::on_restoreButton_clicked()
+{
+
+    if (updater->oldClientExists())
+    {
+
+        updater->restoreClient();
+        ui->dVersionLabel->setText(updater->getDLClientVersion());
+    }
+    else
+    {
+
+        return;
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "fclientupdater.h"
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +15,16 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void setupWindow();
+
+private slots:
+    void on_updateButton_clicked();
+
+    void on_restoreButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+    FClientUpdater *updater;
 };
 
 #endif // MAINWINDOW_H
